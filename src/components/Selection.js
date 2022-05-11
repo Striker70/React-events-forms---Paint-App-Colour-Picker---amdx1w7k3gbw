297ef3a6068371309f2c710ee55bcc6412073c1e
subhasis <55424474+Striker70@users.noreply.github.com>
2022-05-12 00:02:57 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React from "react";
+import "../styles/Child.css";
 
+export default function Selection({ applyColor }) {
+  const [style, setStyle] = React.useState({ background: "" });
+  return (
+    <div
+      className="fix-box"
+      style={style}
+      onClick={() => {
+        applyColor((color) => {
+          setStyle(color);
+        });
+      }}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+}
